4472c3d311cd38ebd4405481c5a42fc40562d460
Mark Hoek <mark.hoek@solidtoken.nl>
2020-05-05 11:42:33 +0200

    Add a scenario for dealing with specific dependencies

---
+++ SpecFlow.DependencyInjection.Tests/ScenarioDependent.feature
@@ -0,0 +1,6 @@
+﻿Feature: ScenarioDependent
+    Issue #29: Add to ServiceCollection depending of Feature or Scenario
+    https://github.com/solidtoken/SpecFlow.DependencyInjection/issues/29
+
+Scenario: Add dependency specific for Scenario
+    Then verify that specific dependency is correctly injected
---
+++ SpecFlow.DependencyInjection.Tests/ScenarioDependentSteps.cs
@@ -0,0 +1,57 @@
+﻿using BoDi;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace SolidToken.SpecFlow.DependencyInjection.Tests
+{
+    [Binding]
+    public class ScenarioDependentSteps
+    {
+        private readonly ITestService testService;
+
+        //[BeforeScenario]
+        //public void InitializeSpecificTestService()
+        //{
+        //    // here we would need to inject SpecificTestService into the ServiceCollection
+        //    // 1) how do we get to the servicecollection
+        //    var oc = ScenarioContext.Current.GetBindingInstance(typeof(BoDi.IObjectContainer));
+
+        //}
+
+        public ScenarioDependentSteps(ITestService testService)
+        {
+            this.testService = testService;
+        }
+
+        [Then(@"verify that specific dependency is correctly injected")]
+        public void ThenVerifyThatSpecificDependencyIsCorrectlyInjected()
+        {
+            Assert.False(testService.Verify());
+        }
+    }
+
+    public class SpecificTestService : ITestService
+    {
+        public bool Verify()
+        {
+            return false;
+        }
+    }
+
+    [Binding]
+    public class WebDriverSupport
+    {
+        private readonly IObjectContainer objectContainer;
+
+        public WebDriverSupport(IObjectContainer objectContainer)
+        {
+            this.objectContainer = objectContainer;
+        }
+
+        [BeforeScenario]
+        public void InitializeWebDriver()
+        {
+
+        }
+    }
+}
